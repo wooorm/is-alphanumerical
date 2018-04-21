4a16e1e022a17558c6ce0c211dec917d13e85e08
Titus Wormer <tituswormer@gmail.com>
2018-04-21 12:09:25 +0200

    Refactor code-style

--- test.js
+++ test.js
@@ -1,25 +1,25 @@
-'use strict';
+'use strict'
 
-var test = require('tape');
-var alphanumerical = require('.');
+var test = require('tape')
+var alphanumerical = require('.')
 
-test('alphanumerical(character)', function (t) {
-  t.ok(alphanumerical('a'));
-  t.ok(alphanumerical('z'));
-  t.ok(alphanumerical('A'));
-  t.ok(alphanumerical('Z'));
-  t.ok(alphanumerical('0'));
-  t.ok(alphanumerical('9'));
-  t.ok(alphanumerical('a'.charCodeAt(0)));
-  t.ok(alphanumerical('9'.charCodeAt(0)));
-  t.notOk(alphanumerical('\t'));
-  t.notOk(alphanumerical('a'.charCodeAt(0) - 1));
-  t.notOk(alphanumerical('z'.charCodeAt(0) + 1));
-  t.notOk(alphanumerical('A'.charCodeAt(0) - 1));
-  t.notOk(alphanumerical('Z'.charCodeAt(0) + 1));
-  t.notOk(alphanumerical('0'.charCodeAt(0) - 1));
-  t.notOk(alphanumerical('9'.charCodeAt(0) + 1));
-  t.notOk(alphanumerical('💩'));
+test('alphanumerical(character)', function(t) {
+  t.ok(alphanumerical('a'))
+  t.ok(alphanumerical('z'))
+  t.ok(alphanumerical('A'))
+  t.ok(alphanumerical('Z'))
+  t.ok(alphanumerical('0'))
+  t.ok(alphanumerical('9'))
+  t.ok(alphanumerical('a'.charCodeAt(0)))
+  t.ok(alphanumerical('9'.charCodeAt(0)))
+  t.notOk(alphanumerical('\t'))
+  t.notOk(alphanumerical('a'.charCodeAt(0) - 1))
+  t.notOk(alphanumerical('z'.charCodeAt(0) + 1))
+  t.notOk(alphanumerical('A'.charCodeAt(0) - 1))
+  t.notOk(alphanumerical('Z'.charCodeAt(0) + 1))
+  t.notOk(alphanumerical('0'.charCodeAt(0) - 1))
+  t.notOk(alphanumerical('9'.charCodeAt(0) + 1))
+  t.notOk(alphanumerical('💩'))
 
-  t.end();
-});
+  t.end()
+})
