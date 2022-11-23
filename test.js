b74bb3f125790e08f5b23ffb678c1a8b8563703c
Titus Wormer <tituswormer@gmail.com>
2022-11-23 12:43:16 +0100

    Use Node test runner

--- test.js
+++ test.js
@@ -1,7 +1,8 @@
-import test from 'tape'
+import assert from 'node:assert/strict'
+import test from 'node:test'
 import {isAlphanumerical} from './index.js'
 
-test('isAlphanumerical(character)', function (t) {
+test('isAlphanumerical(character)', function () {
   const _0 = 0x30
   const _9 = 0x39
   const A = 0x41
@@ -9,22 +10,20 @@ test('isAlphanumerical(character)', function (t) {
   const a = 0x61
   const z = 0x7a
 
-  t.ok(isAlphanumerical('a'))
-  t.ok(isAlphanumerical('z'))
-  t.ok(isAlphanumerical('A'))
-  t.ok(isAlphanumerical('Z'))
-  t.ok(isAlphanumerical('0'))
-  t.ok(isAlphanumerical('9'))
-  t.ok(isAlphanumerical(a))
-  t.ok(isAlphanumerical(_9))
-  t.notOk(isAlphanumerical('\t'))
-  t.notOk(isAlphanumerical(a - 1))
-  t.notOk(isAlphanumerical(z + 1))
-  t.notOk(isAlphanumerical(A - 1))
-  t.notOk(isAlphanumerical(Z + 1))
-  t.notOk(isAlphanumerical(_0 - 1))
-  t.notOk(isAlphanumerical(_9 + 1))
-  t.notOk(isAlphanumerical('💩'))
-
-  t.end()
+  assert.ok(isAlphanumerical('a'))
+  assert.ok(isAlphanumerical('z'))
+  assert.ok(isAlphanumerical('A'))
+  assert.ok(isAlphanumerical('Z'))
+  assert.ok(isAlphanumerical('0'))
+  assert.ok(isAlphanumerical('9'))
+  assert.ok(isAlphanumerical(a))
+  assert.ok(isAlphanumerical(_9))
+  assert.ok(!isAlphanumerical('\t'))
+  assert.ok(!isAlphanumerical(a - 1))
+  assert.ok(!isAlphanumerical(z + 1))
+  assert.ok(!isAlphanumerical(A - 1))
+  assert.ok(!isAlphanumerical(Z + 1))
+  assert.ok(!isAlphanumerical(_0 - 1))
+  assert.ok(!isAlphanumerical(_9 + 1))
+  assert.ok(!isAlphanumerical('💩'))
 })
