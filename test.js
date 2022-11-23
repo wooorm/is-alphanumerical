81dc619cb282ed27c8b2df456a64a48ec8b8c4c0
Titus Wormer <tituswormer@gmail.com>
2022-11-23 12:41:28 +0100

    Update dev-dependencies

--- test.js
+++ test.js
@@ -2,21 +2,28 @@ import test from 'tape'
 import {isAlphanumerical} from './index.js'
 
 test('isAlphanumerical(character)', function (t) {
+  const _0 = 0x30
+  const _9 = 0x39
+  const A = 0x41
+  const Z = 0x5a
+  const a = 0x61
+  const z = 0x7a
+
   t.ok(isAlphanumerical('a'))
   t.ok(isAlphanumerical('z'))
   t.ok(isAlphanumerical('A'))
   t.ok(isAlphanumerical('Z'))
   t.ok(isAlphanumerical('0'))
   t.ok(isAlphanumerical('9'))
-  t.ok(isAlphanumerical('a'.charCodeAt(0)))
-  t.ok(isAlphanumerical('9'.charCodeAt(0)))
+  t.ok(isAlphanumerical(a))
+  t.ok(isAlphanumerical(_9))
   t.notOk(isAlphanumerical('\t'))
-  t.notOk(isAlphanumerical('a'.charCodeAt(0) - 1))
-  t.notOk(isAlphanumerical('z'.charCodeAt(0) + 1))
-  t.notOk(isAlphanumerical('A'.charCodeAt(0) - 1))
-  t.notOk(isAlphanumerical('Z'.charCodeAt(0) + 1))
-  t.notOk(isAlphanumerical('0'.charCodeAt(0) - 1))
-  t.notOk(isAlphanumerical('9'.charCodeAt(0) + 1))
+  t.notOk(isAlphanumerical(a - 1))
+  t.notOk(isAlphanumerical(z + 1))
+  t.notOk(isAlphanumerical(A - 1))
+  t.notOk(isAlphanumerical(Z + 1))
+  t.notOk(isAlphanumerical(_0 - 1))
+  t.notOk(isAlphanumerical(_9 + 1))
   t.notOk(isAlphanumerical('💩'))
 
   t.end()
